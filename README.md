2c0e0b5e9a9ba088ab82f12828b6e1e36ff2c167
ivan-grozni-2 <3ivangrozni@gmail.com>
2025-05-05 19:48:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# To-Do App 
+
+A simple to-do list app built with HTML, CSS, and JavaScript.
+
+## Features
+- Add and delete tasks
+- Saves tasks using `localStorage`
+
+## Demo
+[Live App](https://ivan-grozni-2.github.io/todo-app/)
+
+## Tech Used
+- HTML
+- CSS
+- JavaScript (ES6)
---
+++ script.js
@@ -0,0 +1,55 @@
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
+
+let tasks = [];
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const task = input.value.trim();
+    if(task !== ''){
+        tasks.push(task);
+        inputvalue='';
+        savetask();
+        writetask();
+    }
+});
+
+function writetask(t){
+    list.innerHTML='';
+    tasks.forEach((t, index) => {
+           
+        const li =document.createElement('li')
+        li.innerHTML =`
+            <span>${t}</span>
+            <button class = "delete-button" data-index="${index}"> Delete </button>`;
+        list.appendChild(li);
+        });
+        
+    document.querySelectorAll('.delete-button').forEach(button => {
+        button.addEventListener('click', (e) => {
+            const index = e.target.getAttribute('data-index');
+            tasks.splice(index, 1);
+            savetask();
+            writetask();
+
+    
+        })
+    })
+   
+}
+
+function savetask(){
+    localStorage.setItem('tasks.txt',JSON.stringify(tasks));
+
+}
+
+function loadtasks(){
+    const storedtasks = localStorage.getItem('tasks.txt');
+    if (storedtasks) {
+        tasks = JSON.parse(storedtasks);
+        writetask();
+    }
+}
+
+loadtasks();
